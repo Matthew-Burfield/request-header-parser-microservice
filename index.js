1d9234dbd0cb5396f723ab86ee71ee86dd664906
Matthew-Burfield <burfie@hotmail.com>
2016-12-01 12:31:43 +0000

    added correct output made up from req.headers

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var exports = module.exports = {};
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/*', function(req, res){
+  var ip = req.headers['x-forwarded-for'] || 
+           req.connection.remoteAddress || 
+           req.socket.remoteAddress ||
+           req.connection.socket.remoteAddress,
+      language = req.headers["accept-language"],
+      software = req.headers["user-agent"],
+      indexStart = software.indexOf('(') + 1,
+      indexFinish = software.indexOf(')');
+  var output = {
+    "ipaddress": ip,
+    "language": language.substr(0, language.indexOf(',', 0)),
+    "software": software.substring(indexStart, indexFinish)
+  }
+  
+  
+  res.send(output);
+});
 
-app.listen(process.env.PORT || 8080, function () {
-  console.log('Example app listening on port 3000!')
-})
+var server = app.listen(8080, function(){
+  console.log('Magic is happening on port 8080');
+});
+
+exports.closeServer = function(){
+  server.close();
+};
